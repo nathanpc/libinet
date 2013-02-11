d4e6956a62699554880bbb9aa8f10d27cda1811b
Nathan Campos <eeepc904@gmail.com>
2013-02-11 14:46:22 -0200

    Added examples

---
+++ examples/example-socket.cpp
@@ -0,0 +1,32 @@
+/**
+ * example-socket.cpp
+ * An example of how to use sockets with the awesome libinet++ library.
+ *
+ * \author Nathan Campos
+ */
+
+#include <iostream>
+#include <string>
+
+#include "../src/sockets.h"
+using namespace std;
+
+bool socket_data_callback(string data) {
+	// If something went wrong while you were parsing return false!
+	cout << data << endl;
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+	// Setup the socket and connect.
+	Socket socket("localhost", 80, &socket_data_callback);
+
+	// Send some information.
+	socket.send_data("GET / HTTP/1.1\r\n");
+	socket.send_data("Host: localhost\r\n\r\n");
+
+	// Get the response.
+	socket.receive();
+
+	return 0;
+}
