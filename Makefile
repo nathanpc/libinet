b940e44d4221bfc2ace724e45fa441fa116362f9
Nathan Campos <eeepc904@gmail.com>
2013-02-15 15:48:19 -0200

    #5 - Created a example for the URI usage

--- Makefile
+++ Makefile
@@ -2,13 +2,17 @@ CXX = clang++
 CXXFLAGS = -Wall #-pthread
 SOCKET_OBJ = src/sockets.o examples/example-socket.o
 HTTP_OBJ = src/sockets.o src/http.o examples/example-http.o
+URI_OBJ = src/uri.o examples/example-uri.o
 
-all: example-socket example-http
+all: example-socket example-http example-uri
 
 example-socket: $(SOCKET_OBJ)
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
 example-http: $(HTTP_OBJ)
+	$(CXX) $(CXXFLAGS) $^ -o $@
+
+example-uri: $(URI_OBJ)
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
 debug: CXXFLAGS += -g3 -DDEBUG
---
+++ examples/example-uri.cpp
@@ -0,0 +1,33 @@
+/**
+ * example-uri.cpp
+ * An example of how to use the URI class from the awesome libinet++ library.
+ *
+ * \author Nathan Campos
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../src/uri.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	URI uri("http://test.com/awesome.php?testing=true&another=test");
+
+	// Print the protocol.
+	cout << "Protocol: " << uri.protocol << endl;
+
+	// Print the server and its port.
+	cout << "Server: " << uri.server << endl << "Port: " << uri.port << endl;
+
+	// Print the location.
+	cout << "Location: " << uri.location << endl;
+
+	// Print all the params.
+	for (size_t i = 0; i < uri.params.size(); i++) {
+		cout << "Param: " << uri.params[i][0] << " = " << uri.params[i][1] << endl;
+	}
+
+	return 0;
+}
