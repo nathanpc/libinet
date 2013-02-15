70d35aeff0798142129a902d733145c308e26620
Nathan Campos <eeepc904@gmail.com>
2013-02-15 15:47:09 -0200

    #5 - Implemented the URI class

---
+++ src/uri.cpp
@@ -0,0 +1,97 @@
+/**
+ *  uri.cpp
+ *  URI parsing helper class.
+ *
+ *  \author Nathan Campos <nathanpc@dreamintech.net>
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include "uri.h"
+using namespace std;
+
+
+URI::URI() {
+	uri = "";
+}
+
+URI::URI(string _uri) {
+	uri = _uri;
+	parse();
+}
+
+void URI::parse(string _uri) {
+	uri = _uri;
+	parse();
+}
+
+void URI::parse() {
+	string tmp_uri = string(uri);
+
+	// Parse the protocol
+	if (tmp_uri.find("://") != string::npos) {
+		protocol = tmp_uri.substr(0, tmp_uri.find("://"));
+		tmp_uri = tmp_uri.substr(tmp_uri.find("://") + 3);
+	}
+
+	// Parse the server (and port), and location string, and parameters.
+	if (tmp_uri.find("/") != string::npos) {
+		string tmp_server = tmp_uri.substr(0, tmp_uri.find("/"));
+
+		parse_port(tmp_server);
+		server = tmp_server;
+		tmp_uri = tmp_uri.substr(tmp_uri.find("/") + 1);
+
+		// Check if it has a location.
+		if (!tmp_uri.empty()) {
+			// Check for parameters.
+			if (tmp_uri.find("?") != string::npos) {
+				location = tmp_uri.substr(0, tmp_uri.find("?"));
+				tmp_uri = tmp_uri.substr(tmp_uri.find("?") + 1);
+
+				parse_params(tmp_uri);
+			} else {
+				location = tmp_uri;
+			}
+
+			tmp_uri = "";
+		}
+	} else {
+		parse_port(tmp_uri);
+		server = tmp_uri;
+		location = "/";
+
+		tmp_uri = "";
+	}
+}
+
+void URI::parse_port(string _server) {
+	if (_server.find(":") != string::npos) {
+		port = atoi(_server.substr(_server.find(":") - 1).c_str());
+		_server = _server.substr(0, _server.find(":"));
+	} else {
+		// TODO: Get default port according to the protocol.
+		port = 80;
+	}
+}
+
+void URI::parse_params(string _params) {
+	while (_params.find("&") != string::npos) {
+		string str_param = _params.substr(0, _params.find("&"));
+		parse_param(str_param);
+
+		_params = _params.substr(_params.find("&") + 1);
+	}
+
+	parse_param(_params);
+}
+
+void URI::parse_param(string str_param) {
+	vector<string> param;
+
+	param.push_back(str_param.substr(0, str_param.find("=")));
+	param.push_back(str_param.substr(str_param.find("=") + 1));
+	params.push_back(param);
+}
---
+++ src/uri.h
@@ -0,0 +1,40 @@
+/**
+ *  uri.h
+ *  URI parsing helper class.
+ *
+ *  \author Nathan Campos <nathanpc@dreamintech.net>
+ */
+
+#ifndef LIBINET_URI_H_
+#define LIBINET_URI_H_
+
+#include <string>
+#include <vector>
+
+class URI {
+	private:
+		void parse();
+
+		void parse_port(std::string _server);
+		void parse_params(std::string _params);
+
+		void parse_param(std::string str_param);
+
+	public:
+		std::string uri;
+
+		std::string protocol;
+		std::string server;
+		unsigned int port;
+
+		std::string location;
+		std::vector<std::vector<std::string> > params;
+
+		// Constructors
+		URI();
+		URI(std::string _uri);
+
+		void parse(std::string _uri);
+};
+
+#endif
